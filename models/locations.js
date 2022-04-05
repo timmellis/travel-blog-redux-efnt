f3424c45859c1b9f4134341312be071fdec2da5c
unknown <sbandy9210@gmail.com>
2022-04-05 15:38:05 -0500

    seeded to mongoDb

--- models/locations.js
+++ models/locations.js
@@ -1,12 +1,14 @@
 const mongoose = require ('mongoose')
 const Schema = mongoose.Schema
 
-const locations = new Schema(
+const Locations = new Schema(
     {
         name: {type: String, required: true},
         location: {type: String, required: true},
-        likes: {type: Number, required: true}
+        likes: {type: Number, required: true},
+        details: {type: String, required: true},
+        image: {type: String, required: true}
 
     }
 )
-module.exports = locations
+module.exports = Locations
