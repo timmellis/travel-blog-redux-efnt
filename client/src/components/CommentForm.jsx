92803505673a42a4d9cbcc4a35a9415786f1c2cf
timmellis <tim.m.ellis@gmail.com>
2022-04-05 14:57:35 -0400

    Comment Form component v1

---
+++ client/src/components/CommentForm.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const mapStateToProps = ({  }) => {
+  return {  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+
+const CommentForm = (props) => {
+
+
+return (
+    <div>
+      <h1>Comment Form</h1>
+      <form>
+        Name: <input type='text' name='newName' value={props.newName} onChange={props.handleChange} />
+        Comment: <input type='text' name='newComment' value={props.newComment} onChange={props.handleChange} />
+        <button type='submit' onClick={props.handleSubmit}>Submit</button>
+
+      </form>  
+    </div>
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentForm)
