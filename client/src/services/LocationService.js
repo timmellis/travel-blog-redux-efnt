1c845d99a154d46ac79d4f41cb3f50206aef25e6
timmellis <tim.m.ellis@gmail.com>
2022-04-06 10:40:19 -0400

    added guard operator to Locations component

--- client/src/services/LocationService.js
+++ client/src/services/LocationService.js
@@ -3,7 +3,7 @@ import Client from './'
 export const GetLocations = async () => {
   try {
     const res = await Client.get('/locations')
-
+    
     return res.data
   } catch (error) {
     throw error
