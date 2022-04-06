b4e1a0f810aa660c87925ba881477426541de735
Frank Marra <frankrmarra@gmail.com>
2022-04-06 12:19:45 -0400

    comments now display

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 // import {Link} from 'react-router-dom'
-import { LoadComments } from '../store/actions/LocationActions'
+import { GetAllComments } from '../store/actions/CommentActions'
 
 const mapStateToProps = ({ commentState }) => {
   return { commentState }
@@ -9,7 +9,7 @@ const mapStateToProps = ({ commentState }) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchComments: () => dispatch(LoadComments())
+    fetchComments: () => dispatch(GetAllComments())
   }
 }
 
@@ -17,13 +17,24 @@ const Comments = (props) => {
   useEffect(() => {
     props.fetchComments()
   }, [])
-  console.log(props)
-  return (
-    <div>
-      <p>This is the Comments component.</p>
-      <ul></ul>
-    </div>
-  )
+
+  if (!props || !props.commentState.comments) {
+    return <div>Now loading...</div>
+  } else {
+    return (
+      <div>
+        <p>This is the Comments component.</p>
+        <div className="user-comments">
+          {props.commentState.comments.map((comment) => (
+            <div className="user-comment-wrapper" key={comment._id}>
+              <div className="user-name">{comment.userName}</div>
+              <div className="user-comment">{comment.comment}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Comments)
