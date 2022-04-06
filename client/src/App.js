81427de857cab0b417cad859388813a320e1c0b6
Frank Marra <frankrmarra@gmail.com>
2022-04-06 11:28:26 -0400

    comments working

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Locations from './components/Locations'
 import LocationDetails from './components/LocationDetails'
+import CommentForm from './components/CommentForm'
+import Comments from './components/Comments'
 
 function App() {
   return (
     <div className="App">
-     <h1>Exotic Travel Locations!</h1>
-     <div className='flex-row'>
-       
-       <div className='left'>
-         <Locations />
-       </div>
+      <h1>Exotic Travel Locations!</h1>
+      <div className="flex-row">
+        <div className="left">
+          <Locations />
+        </div>
 
-      <div className='right'>
-        <Routes>
-          <Route path='/locations/:id' components={<LocationDetails />} />
-        </Routes>
-
+        <div className="right">
+          <Routes>
+            <Route path="/locations/:id" element={<LocationDetails />} />
+            <Route path="/commentform" element={<CommentForm />} />
+            <Route path="/comments" element={<Comments />} />
+          </Routes>
+        </div>
       </div>
-
-     </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- controllers/index.js
+++ controllers/index.js
@@ -1,66 +1,65 @@
-const { Comment, Locations }  = require('../models')
-
+const { Comment, Locations } = require('../models')
 
 const createComm = async (req, res) => {
-    try {
-        const comment = await new Comment(req.body)
-        await comment.save()
-        return res.status(201).json({
-            comment
-        });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
+  console.log('req.body', req.body)
+  try {
+    const comment = await new Comment(req.body)
+    await comment.save()
+    return res.status(201).json(comment)
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
 }
 
 const getAllComms = async (req, res) => {
-    try {
-        const comments = await Comments.find()
-        return res.status(200).json({ comments })
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+  try {
+    const comments = await Comments.find()
+    return res.status(200).json({ comments })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 const getAllLocs = async (req, res) => {
-    try {
-        const locations = await Locations.find()
-        return res.status(200).json({ locations })
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+  try {
+    const locations = await Locations.find()
+    return res.status(200).json({ locations })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 const getLocById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const location = await Locations.findById(id)
-        if (location) {
-            return res.status(200).json({ location });
-        }
-        return res.status(404).send('Location with the specified ID does not exists');
-    } catch (error) {
-        return res.status(500).send(error.message);
+  try {
+    const { id } = req.params
+    const location = await Locations.findById(id)
+    if (location) {
+      return res.status(200).json({ location })
     }
+    return res
+      .status(404)
+      .send('Location with the specified ID does not exists')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 const createLoc = async (req, res) => {
-    try {
-        const location = await new Locations(req.body)
-        await location.save()
-        return res.status(201).json({
-            location
-        });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
+  try {
+    const location = await new Locations(req.body)
+    await location.save()
+    return res.status(201).json({
+      location
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
 }
 
-
 module.exports = {
-    createComm,
-    getAllLocs,
-    getLocById,
-    createLoc,
-    getAllComms
+  createComm,
+  getAllLocs,
+  getLocById,
+  createLoc,
+  getAllComms
 }
